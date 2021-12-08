12ce2240a01b1fb1e507d61676901f5141d26dc8
marc_bayart <marc_bayart@ens.univ-artois.fr>
2021-12-08 14:05:17 +0100

    feat: Ajout menu

---
+++ main.py
@@ -0,0 +1,34 @@
+print("Bienvenue dans le jeu des gobelets !")
+print()
+print("1) Nouvelle partie")
+print("2) Options")
+print("3) Crédits")
+print("4) Quitter")
+print()
+
+choix = int(input("Entrez le chiffre correspondant à votre choix : "))
+print()
+
+while (choix!=4):
+    if (choix==1):
+        print("Nouvelle partie")
+    elif (choix==2):
+        print("Options")
+    elif (choix==3):
+        print("Crédits :")
+        print("Développeur : Marc BAYART")
+        print("Version de Python utilisée : 3.10")
+    else:
+        print("Votre choix est invalide !")
+
+    print()
+    print("Bienvenue dans le jeu des gobelets !")
+    print()
+    print("1) Nouvelle partie")
+    print("2) Options")
+    print("3) Crédits")
+    print("4) Quitter")
+    print()
+
+    choix = int(input("Entrez le chiffre correspondant à votre choix : "))
+    print()
